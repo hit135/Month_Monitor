c8831151e674410bcc91e4601ff1f335b00a3f6a
zerojun <sunzerot@gmail.com>
2021-07-15 15:30:51 +0900

    area 리스트 작업 완료

--- frontend/src/views/area/areaMgr.js
+++ frontend/src/views/area/areaMgr.js
@@ -1,23 +1,63 @@
 import React, {useEffect, useState} from 'react'
 import {getAreaList, getData} from "../../agent/area";
+
+import {
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CRow,
+} from  '@coreui/react'
+import PageTableWidget from "../../widget/pageTableWidget";
 function AreaMgr() {
   const columns = [
-    { dataField: 'rowNum',     text: '번호',     headerStyle: {textAlign: 'center', width: '10%', height: '35px'} },
-    { dataField: 'strCode',    text: '상점코드', headerStyle: {textAlign: 'center', width: '30%'} },
-    { dataField: 'strName',    text: '상점명',   headerStyle: {textAlign: 'center'} },
+    { dataField: 'areaOrder', text: '출력순서', headerStyle: {textAlign: 'center'} },
+    { dataField: 'areaName', text: '구역명', headerStyle: {textAlign: 'center'},  },
+    { dataField: 'areaCode', text: '구역코드', headerStyle: {textAlign: 'center'}, },
+    { dataField: 'storeCnt', text: '소속상점수', headerStyle: {textAlign: 'center'}, },
+    { dataField: 'areaManager', text: '구역관리자', headerStyle: {textAlign: 'center'}, },
+    { dataField: 'areaTel', text: '전화번호', headerStyle: {textAlign: 'center'}, },
+    { dataField: 'useYn', text: '사용유무', headerStyle: {textAlign: 'center'}, },
+    { dataField: 'delYn', text: '삭제유무', headerStyle: {textAlign: 'center'}, },
+    { dataField: 'regDate', text: '등록일자', headerStyle: {textAlign: 'center'},},
   ];
 
   const [repo, setRepo] = useState([]);
+  const page = 1;
   useEffect(() => {
     getAreaList().then(function(resp){
-        console.log(resp);
+      console.log(resp);
+        setRepo(resp.data["resultList"]);
       }
     );
 
   }, []);
   return (
     <>
-
+      <CRow>
+        <CCol>
+          <CCard>
+            <CCardHeader>
+              <CCol md="12" xl="12">
+                <h5 className={"mb-0 ml-0"}>전체 시장 목록</h5>
+              </CCol>
+            </CCardHeader>
+            <CCardBody>
+              <PageTableWidget
+                keyField={"areaCode"}
+                data={repo}
+                page={page}
+                // sizePerPage={sizePerPage}
+                // totalSize={totalElementsCount}
+                // onTableChange={this.handleTableChange}
+                // onSizePerPageChange={this.handleSizePerPageChange}
+                viewColumns={columns}
+                // selectRow={selectRowProp}
+              />
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
     </>
   )
 }
--- frontend/src/widget/pageTableWidget.js
+++ frontend/src/widget/pageTableWidget.js
@@ -1,40 +1,37 @@
 import React, {useEffect, useState} from 'react'
-import BootstrapTable from "react-bootstrap-table-2";
+import BootstrapTable from 'react-bootstrap-table-next';
 import paginationFactory from "react-bootstrap-table2-paginator";
 
+//
 function PageTableWidget(props) {
-
-  // const {
-  //   keyField, data, page, sizePerPage, onTableChange, onSizePerPageChange, totalSize, viewColumns, selectRow, paginationSize, cellEdit,
-  // } = this.props;
-
-  const routerProps = {
-    keyField : props.keyField
-  }
-
+//
+  const {
+    keyField, data, page, sizePerPage, onTableChange, onSizePerPageChange, totalSize, viewColumns, selectRow, paginationSize, cellEdit,
+  } = props;
+//
   return (
     <>
-      {/*<BootstrapTable*/}
-      {/*  keyField={routerProps.keyField}*/}
-      {/*  // data={data.slice()}*/}
-      {/*  columns={viewColumns}*/}
-      {/*  onTableChange={onTableChange}*/}
-      {/*  rowEvents={selectRow}*/}
-      {/*  remote*/}
-      {/*  striped*/}
-      {/*  hover*/}
-      {/*  condensed*/}
-      {/*  pagination={paginationFactory({*/}
-      {/*    page,*/}
-      {/*    sizePerPage,*/}
-      {/*    totalSize,*/}
-      {/*    paginationSize: (paginationSize) ? paginationSize : 5,*/}
-      {/*    showTotal: false,*/}
-      {/*    hidePageListOnlyOnePage: true,*/}
-      {/*    hideSizePerPage: true,*/}
-      {/*    onSizePerPageChange: onSizePerPageChange,*/}
-      {/*  })}*/}
-      {/*  noDataIndication={() => <div style={{textAlign: 'center'}}>목록이 없습니다.</div>} />*/}
+      <BootstrapTable
+        keyField={keyField}
+        data={data.slice()}
+        columns={viewColumns}
+        // onTableChange={onTableChange}
+        // rowEvents={selectRow}
+        remote
+        striped
+        hover
+        condensed
+        // pagination={paginationFactory({
+        //   page,
+        //   sizePerPage,
+        //   totalSize,
+        //   paginationSize: (paginationSize) ? paginationSize : 5,
+        //   showTotal: false,
+        //   hidePageListOnlyOnePage: true,
+        //   hideSizePerPage: true,
+        //   onSizePerPageChange: onSizePerPageChange,
+        // })}
+        noDataIndication={() => <div style={{textAlign: 'center'}}>목록이 없습니다.</div>} />
     </>
   )
 }
