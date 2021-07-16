b2fb17985092397592b2cf86e3975c36190758b7
hyunminuhm <hyunminuhm@gmail.com>
2021-07-16 13:50:39 +0900

    - 발송이력 화면 작업 중 - package.json : engines -> yarn 추가

--- frontend/src/views/push/pushMgr.js
+++ frontend/src/views/push/pushMgr.js
@@ -1,11 +1,25 @@
 import React, { lazy } from 'react'
+import { CRow, CCol, CButtonGroup, CButton, CInput, CSelect } from '@coreui/react'
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.min.css';
+import { ko } from "date-fns/esm/locale";
 
 const PushMgr = () => {
   return (
-    <>
+    <div>
+      <div>
+        <CButtonGroup>
+          <CButton color={'info'}>전체</CButton>
+          <CButton color={'info'}>발송</CButton>
+          <CButton color={'info'}>미발송</CButton>
+        </CButtonGroup>
+      </div>
+      <div>
 
-    </>
+
+      </div>
+    </div>
   )
 }
 
-export default PushMgr
+export default PushMgr;
