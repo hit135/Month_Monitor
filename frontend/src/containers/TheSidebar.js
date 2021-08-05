925fbb911dbb76ad949eb4d6a649b5ff02ea6f06
zerojun <sunzerot@gmail.com>
2021-08-05 20:39:26 +0900

    mng adm 메뉴 이동 생성 및 favicon, 타이틀 수정

--- frontend/src/containers/TheSidebar.js
+++ frontend/src/containers/TheSidebar.js
@@ -17,6 +17,14 @@ import CIcon from '@coreui/icons-react'
 // sidebar nav config
 import navigation from './_nav'
 
+function movePageMng() {
+  if(window.location.hostname === "localhost") {
+    window.location.href = "http://localhost:8081/";
+  } else {
+    window.location.href = "http://1.223.40.19:30081/";
+  }
+}
+
 const TheSidebar = () => {
   const dispatch = useDispatch()
   const show = useSelector(state => state.sidebarShow)
@@ -27,19 +35,10 @@ const TheSidebar = () => {
       onShowChange={(val) => dispatch({type: 'set', sidebarShow: val })}
     >
       <CSidebarBrand className="d-md-down-none" to="/">
-        <CIcon
-          className="c-sidebar-brand-full"
-          name="logo-negative"
-          height={35}
-        />
-        <CIcon
-          className="c-sidebar-brand-minimized"
-          name="sygnet"
-          height={35}
-        />
+        <img src={'logo-fs.png'} style={{ "width" : "3rem"}} />
+        <span style={{"font-size" : "1.2rem", fontWeight : "900"}}>관제관리시스템</span>
       </CSidebarBrand>
       <CSidebarNav>
-
         <CCreateElement
           items={navigation}
           components={{
@@ -49,6 +48,13 @@ const TheSidebar = () => {
             CSidebarNavTitle
           }}
         />
+        <li className="c-sidebar-nav-item"><a className="c-sidebar-nav-link" tabIndex="0" onClick={movePageMng}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="c-sidebar-nav-icon" role="img">
+            <polygon fill="var(--ci-primary-color, currentColor)"
+                     points="359.873 121.377 337.246 144.004 433.243 240.001 16 240.001 16 240.002 16 272.001 16 272.002 433.24 272.002 337.246 367.996 359.873 390.623 494.498 256 359.873 121.377"
+                     className="ci-primary"></polygon>
+          </svg>
+          전체관제이동</a></li>
       </CSidebarNav>
       <CSidebarMinimizer className="c-d-md-down-none"/>
     </CSidebar>
--- frontend/src/containers/_nav.js
+++ frontend/src/containers/_nav.js
@@ -1,86 +1,58 @@
 import React from 'react'
 import CIcon from '@coreui/icons-react'
 
+const a = () => {
+  console.log(1234);
+}
+
 const _nav =  [
   {
     _tag: 'CSidebarNavItem',
     name: '구역관리',
     to: '/area',
     icon: <CIcon name="cil-home" customClasses="c-sidebar-nav-icon"/>,
-    // badge: {
-    //   color: 'info',
-    //   text: 'NEW',
-    // }
   },
   {
     _tag: 'CSidebarNavItem',
     name: '회원관리',
     to: '/mem',
     icon: <CIcon name="cil-user" customClasses="c-sidebar-nav-icon"/>,
-    // badge: {
-    //   color: 'info',
-    //   text: 'NEW',
-    // }
   },
   {
     _tag: 'CSidebarNavItem',
     name: '상점관리',
     to: '/str',
     icon: <CIcon name="cil-basket" customClasses="c-sidebar-nav-icon"/>,
-    // badge: {
-    //   color: 'info',
-    //   text: 'NEW',
-    // }
   },
   {
     _tag: 'CSidebarNavItem',
     name: '센서관리',
     to: '/snsr',
     icon: <CIcon name="cil-speedometer" customClasses="c-sidebar-nav-icon"/>,
-    // badge: {
-    //   color: 'info',
-    //   text: 'NEW',
-    // }
   },
   {
     _tag: 'CSidebarNavItem',
     name: '점검자관리',
     to: '/inspr',
     icon: <CIcon name="cil-user" customClasses="c-sidebar-nav-icon"/>,
-    // badge: {
-    //   color: 'info',
-    //   text: 'NEW',
-    // }
   },
   {
     _tag: 'CSidebarNavItem',
     name: '점검관리',
     to: '/insp',
     icon: <CIcon name="cil-list" customClasses="c-sidebar-nav-icon"/>,
-    // badge: {
-    //   color: 'info',
-    //   text: 'NEW',
-    // }
   },
   {
     _tag: 'CSidebarNavItem',
     name: '발송이력',
     to: '/push',
     icon: <CIcon name="cil-bell" customClasses="c-sidebar-nav-icon"/>,
-    // badge: {
-    //   color: 'info',
-    //   text: 'NEW',
-    // }
   },
   {
     _tag: 'CSidebarNavItem',
     name: '통계',
     to: '/stat',
     icon: <CIcon name="cil-chart-pie" customClasses="c-sidebar-nav-icon"/>,
-    // badge: {
-    //   color: 'info',
-    //   text: 'NEW',
-    // }
   },
 ]
 
