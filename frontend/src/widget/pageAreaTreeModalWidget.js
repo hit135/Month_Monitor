89ba047d2f51a63f4c8c9f23b308771211a690e5
zerojun <sunzerot@gmail.com>
2021-08-13 19:17:17 +0900

    구역 위젯 오류 수정

--- frontend/src/widget/pageAreaTreeModalWidget.js
+++ frontend/src/widget/pageAreaTreeModalWidget.js
@@ -7,25 +7,10 @@ let gData = [];
 
 function PageAreaTreeModalWidget(props) {
   const {onAreaModal, setOnAreaModal, nodeClick, initAreaCode} = props;
-  const [initTree, setInitTree] = useState();
   const [expandedKeys, setExpandedKeys] = useState([]);
   const [autoExpandParent, setAutoExpandParent] = useState(true);
   const [searchValue, setSearchValue] = useState("");
   const [inPutSearchValue, setInputSearchValue] = useState("");
-
-  const initTreeData = () => {
-    setInitTree(
-      <Tree
-        showLine={true}
-        onExpand={onExpand}
-        expandedKeys={expandedKeys}
-        autoExpandParent={autoExpandParent}
-        onClick={nodeClick}
-        treeData={loop(gData)}
-        filterTreeNode={filterTreeNode}
-      />
-    )
-  }
 
   const onExpand = (expandedKeys) => {
     setExpandedKeys(expandedKeys);
@@ -58,14 +43,6 @@ function PageAreaTreeModalWidget(props) {
       setAutoExpandParent(false);
     }
   };
-
-  useEffect(() => {
-    if(onAreaModal) {
-      initTreeData();
-    } else {
-      setInitTree("");
-    }
-  }, [onAreaModal])
 
   useEffect(() => {
     handleInitTree().then(r => {
@@ -130,7 +107,15 @@ function PageAreaTreeModalWidget(props) {
           </CCol>
           <CRow className={"mb-3"}>
           </CRow>
-          {initTree}
+          <Tree
+            showLine={true}
+            onExpand={onExpand}
+            expandedKeys={expandedKeys}
+            autoExpandParent={autoExpandParent}
+            onClick={nodeClick}
+            treeData={loop(gData)}
+            filterTreeNode={filterTreeNode}
+          />
         </CModalBody>
         <CModalFooter>
           <CButton color="primary" onClick={initAreaCode}>초기화</CButton>
