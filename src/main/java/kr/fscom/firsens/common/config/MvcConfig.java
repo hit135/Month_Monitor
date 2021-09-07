c9b38b37b6e131d69dbdfabed4ffd9209f51a3d6
hyunminuhm <hyunminuhm@gmail.com>
2021-09-07 18:16:26 +0900

    sameSiteCookiesConfig 추가

--- src/main/java/kr/fscom/firsens/common/config/MvcConfig.java
+++ src/main/java/kr/fscom/firsens/common/config/MvcConfig.java
@@ -1,12 +1,18 @@
 package kr.fscom.firsens.common.config;
 
+import org.apache.tomcat.util.http.Rfc6265CookieProcessor;
+import org.apache.tomcat.util.http.SameSiteCookies;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.resource.PathResourceResolver;
-
-import java.io.IOException;
 
 /**
  * Spring Boot Mvc 설정
@@ -85,6 +91,15 @@ public class MvcConfig implements WebMvcConfigurer {
                 .setCachePeriod(3600)
                 .resourceChain(true)
                 .addResolver(new PathResourceResolver());
+    }
+
+    @Bean
+    public TomcatContextCustomizer sameSiteCookiesConfig() {
+        return context -> {
+            final Rfc6265CookieProcessor cookieProcessor = new Rfc6265CookieProcessor();
+            cookieProcessor.setSameSiteCookies(SameSiteCookies.NONE.getValue());
+            context.setCookieProcessor(cookieProcessor);
+        };
     }
 
 }
