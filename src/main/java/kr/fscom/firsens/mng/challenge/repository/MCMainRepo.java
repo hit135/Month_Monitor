5257054a03c9b6107bc55a92c2636475708b66ba
hyunminuhm <hyunminuhm@gmail.com>
2021-11-22 14:23:48 +0900

    인천 repository 추가 중

--- src/main/java/kr/fscom/firsens/mng/challenge/repository/MCMainRepo.java
+++ src/main/java/kr/fscom/firsens/mng/challenge/repository/MCMainRepo.java
@@ -30,19 +30,19 @@ public interface MCMainRepo {
     List<HashMap<String, Object>> LIST_MCM_SENSOR_CNT(Map<String, Object> param) throws Exception;                   // 종합 현황 (전체, 경고, 주의, 끊김)
     List<HashMap<String, Object>> LIST_MCM_AREA(Map<String, Object> param) throws Exception;                         // 구별, 시장별 센서 현황 (상점, 센서, 경고, 주의, 끊김)
     List<HashMap<String, Object>> LIST_MCM_SENSOR(Map<String, Object> param) throws Exception;                       // 상태이상 센서 목록
-
     List<HashMap<String, Object>> LIST_MCM_AREA_SENSOR_CNT(Map<String, Object> param) throws Exception;              // 시장 현황 (전체, 경고, 주의, 끊김)
+
     List<HashMap<String, Object>> LIST_MCM_AREA_STORE(Map<String, Object> param) throws Exception;                   // 시장 내 상점 현황 (경고, 주의, 끊김, 센서)
     List<HashMap<String, Object>> LIST_MCM_AREA_SENSOR(Map<String, Object> param) throws Exception;                  // 시장 내 센서 현황 (경고, 주의, 끊김, 점검)
     
     List<HashMap<String, Object>> LIST_MCM_STORE_SEARCH(Map<String, Object> param) throws Exception;                 // 상점 검색 목록
     List<HashMap<String, Object>> LIST_MCM_CHECK_SENSOR(Map<String, Object> param) throws Exception;                 // 점검내역 목록
 
-    List<HashMap<String, Object>> LIST_MCM_AREA_DATA_CHART(Map<String, Object> param) throws Exception;         // 시장별 어제/오늘 데이터 추이
-    List<HashMap<String, Object>> LIST_MCM_AREA_LOG_CHART(Map<String, Object> param) throws Exception;          // 시장별 어제/오늘 경보 발생 추이
-    List<HashMap<String, Object>> LIST_MCM_AREA_DATA_CHART_WEEK(Map<String, Object> param) throws Exception;    // 시장별 주간 데이터 추이
-    List<HashMap<String, Object>> LIST_MCM_AREA_LOG_CHART_WEEK(Map<String, Object> param) throws Exception;     // 시장별 주간 경보 발생 추이
+    List<HashMap<String, Object>> LIST_MCM_AREA_DATA_CHART(Map<String, Object> param) throws Exception;              // 시장별 어제/오늘 데이터 추이
+    List<HashMap<String, Object>> LIST_MCM_AREA_LOG_CHART(Map<String, Object> param) throws Exception;               // 시장별 어제/오늘 경보 발생 추이
+    List<HashMap<String, Object>> LIST_MCM_AREA_DATA_CHART_WEEK(Map<String, Object> param) throws Exception;         // 시장별 주간 데이터 추이
+    List<HashMap<String, Object>> LIST_MCM_AREA_LOG_CHART_WEEK(Map<String, Object> param) throws Exception;          // 시장별 주간 경보 발생 추이
 
-    List<HashMap<String, Object>> LIST_MCM_AREA_MAP_SENSOR(Map<String, Object> param) throws Exception;         // 시장 지도 정보
+    List<HashMap<String, Object>> LIST_MCM_AREA_MAP_SENSOR(Map<String, Object> param) throws Exception;              // 시장 지도 정보
 
 }
--- src/main/java/kr/fscom/firsens/mng/incheon/repository/MIMainRepo.java
+++ src/main/java/kr/fscom/firsens/mng/incheon/repository/MIMainRepo.java
@@ -3,11 +3,17 @@ package kr.fscom.firsens.mng.incheon.repository;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Mapper
 @Repository
 public interface MIMainRepo {
 
-
-
+    List<HashMap<String, Object>> LIST_MIM_SENSOR_CNT(Map<String, Object> param) throws Exception;                   // 종합 현황 (전체, 경고, 주의, 끊김)
+    List<HashMap<String, Object>> LIST_MIM_AREA(Map<String, Object> param) throws Exception;                         // 시장별 센서 현황 (상점, 센서, 경고, 주의, 끊김)
+    List<HashMap<String, Object>> LIST_MIM_SENSOR(Map<String, Object> param) throws Exception;                       // 상태이상 센서 목록
+    List<HashMap<String, Object>> LIST_MIM_AREA_SENSOR_CNT(Map<String, Object> param) throws Exception;              // 시장 현황 (전체, 경고, 주의, 끊김)
 
 }
