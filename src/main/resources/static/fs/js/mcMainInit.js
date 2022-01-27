eae1a8ab24b9bdc26ed2a419f1411801eb5db431
Hyunmin Uhm <hyunminuhm@gmail.com>
2022-01-27 14:25:40 +0900

    숫자 comma 반영

--- src/main/resources/static/fs/js/mcMainInit.js
+++ src/main/resources/static/fs/js/mcMainInit.js
@@ -344,3 +344,7 @@ function fn_mcmi_dateToString(d) {
   return d.getFullYear() + '-' + ('0' + (d.getMonth() + 1)).slice(-2) + '-' + ('0' + d.getDate()).slice(-2) +
     ' ' + ('0' + d.getHours()).slice(-2) + ':' + ('0' + d.getMinutes()).slice(-2) + ':' + ('0' + d.getSeconds()).slice(-2);
 }
+
+function fn_mcmi_numberWithCommas(s) {
+  return s.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
