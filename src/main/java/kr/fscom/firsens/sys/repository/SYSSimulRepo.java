c8d70cec10112b4adc321917ad45311da99cb8e0
hyunminuhm <hyunminuhm@gmail.com>
2022-03-21 17:19:30 +0900

    테스트중

--- src/main/java/kr/fscom/firsens/sys/repository/SYSSimulRepo.java
+++ src/main/java/kr/fscom/firsens/sys/repository/SYSSimulRepo.java
@@ -10,10 +10,11 @@ import java.util.List;
 @Repository
 public interface SYSSimulRepo {
 
+    /*
     int SELECT_SYS_MONTHLY_EVT_GRADE(HashMap<String, Object> param) throws Exception;
     HashMap<String, Object> SELECT_SYS_MONTHLY_RPT(HashMap<String, Object> param) throws Exception;
 
     List<HashMap<String, Object>> LIST_SYS_AREA_PUSH() throws Exception;
-    int INSERT_SYS_PUSH(HashMap<String, Object> param) throws Exception;
+    int INSERT_SYS_PUSH(HashMap<String, Object> param) throws Exception;*/
 
 }
