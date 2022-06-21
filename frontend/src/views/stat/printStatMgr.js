bb8b4c10d746f3d281d0397a45700af730ce7307
hyunminuhm <hyunminuhm@gmail.com>
2022-06-21 18:28:12 +0900

    보고서 년도 위치 수정

--- frontend/src/views/stat/printStatMgr.js
+++ frontend/src/views/stat/printStatMgr.js
@@ -1,48 +1,39 @@
 import React, {useEffect, useRef, useState} from 'react'
-import {
-  CRow,
-  CButton,
-  CButtonGroup,
-  CCard,
-  CCardBody,
-  CCardHeader,
-  CCol,
-  CSelect,
-} from "@coreui/react";
+import {CRow, CButton, CButtonGroup, CCard, CCardBody, CCardHeader, CCol, CSelect} from "@coreui/react";
 import 'react-datepicker/dist/react-datepicker.min.css';
+
 export const ComponentToPrint = React.forwardRef((props, ref) => (
   <div ref={ref}>
     {/* 표지 */}
-    {
-      props.typeName !== "store" &&
+    {props.typeName !== "store" &&
       <div className={"printRow"}>
         <div className={"d-flex justify-content-between logoMargin"}>
           <img src={'/fs/img/logo.png'} style={{ width: "250px" }} />
           <div className={"p-5"}>
-            <h3 style={{fontSize: "40px"}}>
-              {props.type}
-            </h3>
+            <h3 style={{ fontSize: "40px" }}>{props.type}</h3>
           </div>
         </div>
-        <div className={"d-flex justify-content-center"} style={{marginTop: "10rem"}}>
+        <div className={"d-flex justify-content-center"} style={{ marginTop: "10rem" }}>
           <h1 style={{fontSize: "50px"}}>대전 스마트시티 챌리지 사업(20~22)</h1>
         </div>
-        <div className={"d-flex justify-content-center"} style={{marginTop: "5rem"}}>
+        <div className={"d-flex justify-content-center"} style={{ marginTop: "5rem" }}>
           <h1 style={{fontSize: "50px"}}>&lt;전기화재 예방 서비스&gt;</h1>
         </div>
-
-        <div className={"d-flex justify-content-center"} style={{marginTop: "10rem"}}>
-          <h1 className={"text-center"}style={{fontSize: "65px", textDecoration: "underline", textUnderlinePosition: "under"}}>
-            {props.areaTitle} [<span style={{color : "#d24242"}}>{props.endDate === undefined ? props.startDate : props.startDate + " ~ " + props.endDate}</span>]<br/><br/>
+        <div className={"d-flex justify-content-center"} style={{ marginTop: "10rem" }}>
+          <h1 className={"text-center"} style={{ fontSize: "65px", textDecoration: "underline", textUnderlinePosition: "under" }}>
+            {props.areaTitle}<br/><br/>
             관제서비스 운영보고서
           </h1>
         </div>
-
-        <div className={"d-flex justify-content-center"} style={{marginTop: "500px"}}>
-          <h2 style={{fontSize: "50px", fontWeight: "900"}}>㈜에프에스</h2>
+        <div className={"d-flex justify-content-center"} style={{ marginTop: "5rem" }}>
+          <h1 className={"text-center"} style={{ fontSize: "65px", textDecoration: "underline", textUnderlinePosition: "under" }}>
+            [<span style={{ color: "#d24242" }}>{props.startDate + " ~ " + props.endDate}</span>]
+          </h1>
         </div>
-      </div>
-    }
+        <div className={"d-flex justify-content-center"} style={{ marginTop: "500px" }}>
+          <h2 style={{ fontSize: "50px", fontWeight: "900" }}>㈜에프에스</h2>
+        </div>
+      </div>}
 
     <CCard className={"mb-0"}>
       <CCardBody className={"p-0"}>
@@ -51,23 +42,20 @@ export const ComponentToPrint = React.forwardRef((props, ref) => (
           <div className={"d-flex justify-content-between logoMargin"}>
             <img src={'/fs/img/logo.png'} style={{ width: "250px" }} />
             <div className={"p-5"}>
-              <h3 style={{fontSize: "30px"}}>
-                {props.areaTitle} 운영보고서
-              </h3>
+              <h3 style={{ fontSize: "30px" }}>{props.areaTitle} 운영보고서</h3>
             </div>
           </div>
           <div className={"pl-4 pr-4 pt-4"}>
-            <div style={{width: "1650px"}}>
+            <div style={{ width: "1650px" }}>
               {props.areaState}
               {props.areaTotalWarning}
             </div>
           </div>
           <div className="position-absolute-ct">
-            <h3 className="m-0" style={{fontSize: "30px"}}>- 1 -</h3>
+            <h3 className="m-0" style={{ fontSize: "30px" }}>- 1 -</h3>
           </div>
-
           <div className="position-absolute-rb">
-            <img src={"/fs/img/logo-fs.png"} style={{width: "100%", maxWidth: "150px" }} />
+            <img src={"/fs/img/logo-fs.png"} style={{ width: "100%", maxWidth: "150px" }} />
           </div>
         </div>
 
@@ -76,23 +64,17 @@ export const ComponentToPrint = React.forwardRef((props, ref) => (
           <div className={"d-flex justify-content-between logoMargin"}>
             <img src={'/fs/img/logo.png'} style={{ width: "250px" }} />
             <div className={"p-5"}>
-              <h3 style={{fontSize: "30px"}}>
-                {props.areaTitle} 운영보고서
-              </h3>
+              <h3 style={{ fontSize: "30px" }}>{props.areaTitle} 운영보고서</h3>
             </div>
           </div>
           <div className={"pl-4 pr-4 pt-4"}>
-            <div style={{width: "1650px"}}>
-              {props.areaHourlyStat}
-            </div>
+            <div style={{ width: "1650px" }}>{props.areaHourlyStat}</div>
           </div>
-
           <div className="position-absolute-ct">
-            <h3 className="m-0" style={{fontSize: "30px"}}>- 2 -</h3>
+            <h3 className="m-0" style={{ fontSize: "30px" }}>- 2 -</h3>
           </div>
-
           <div className="position-absolute-rb">
-            <img src={"/fs/img/logo-fs.png"} style={{width: "100%", maxWidth: "150px" }} />
+            <img src={"/fs/img/logo-fs.png"} style={{ width: "100%", maxWidth: "150px" }} />
           </div>
         </div>
 
@@ -100,81 +82,55 @@ export const ComponentToPrint = React.forwardRef((props, ref) => (
           <div className={"d-flex justify-content-between logoMargin"}>
             <img src={'/fs/img/logo.png'} style={{ width: "250px" }} />
             <div className={"p-5"}>
-              <h3 style={{fontSize: "30px"}}>
-                {props.areaTitle} 운영보고서
-              </h3>
+              <h3 style={{ fontSize: "30px" }}>{props.areaTitle} 운영보고서</h3>
             </div>
           </div>
           <div className={"pl-4 pr-4 pt-4"}>
-            <div style={{width: "1650px"}}>
-              {props.areaHourlyStat2}
-            </div>
+            <div style={{ width: "1650px" }}>{props.areaHourlyStat2}</div>
           </div>
-
           <div className="position-absolute-ct">
-            <h3 className="m-0" style={{fontSize: "30px"}}>- 3 -</h3>
+            <h3 className="m-0" style={{ fontSize: "30px" }}>- 3 -</h3>
           </div>
-
           <div className="position-absolute-rb">
-            <img src={"/fs/img/logo-fs.png"} style={{width: "100%", maxWidth: "150px" }} />
+            <img src={"/fs/img/logo-fs.png"} style={{ width: "100%", maxWidth: "150px" }} />
           </div>
         </div>
 
-        {
-          props.areaTitle === "대전중앙시장" &&
+        {props.areaTitle === "대전중앙시장" &&
           <div className={"printRow"}>
             <div className={"d-flex justify-content-between logoMargin"}>
               <img src={'/fs/img/logo.png'} style={{ width: "250px" }} />
               <div className={"p-5"}>
-                <h3 style={{fontSize: "30px"}}>
-                  {props.areaTitle} 운영보고서
-                </h3>
+                <h3 style={{ fontSize: "30px" }}>{props.areaTitle} 운영보고서</h3>
               </div>
             </div>
-
-
             <div className={"pl-4 pr-4 pt-4"}>
-              <div style={{width: "1650px"}}>
-                {props.levelAreaStat}
-              </div>
+              <div style={{ width: "1650px" }}>{props.levelAreaStat}</div>
             </div>
             <div className="position-absolute-ct">
-              <h3 className="m-0" style={{fontSize: "30px"}}>- 4 -</h3>
+              <h3 className="m-0" style={{ fontSize: "30px" }}>- 4 -</h3>
             </div>
-
             <div className="position-absolute-rb">
-              <img src={"/fs/img/logo-fs.png"} style={{width: "100%", maxWidth: "150px" }} />
+              <img src={"/fs/img/logo-fs.png"} style={{ width: "100%", maxWidth: "150px" }} />
             </div>
-
-          </div>
-        }
+          </div>}
 
         {/* 내 상점별 현황 */}
         <div className={"printRow"}>
           <div className={"d-flex justify-content-between logoMargin"}>
             <img src={'/fs/img/logo.png'} style={{ width: "250px" }} />
             <div className={"p-5"}>
-              <h3 style={{fontSize: "30px"}}>
-                {props.areaTitle} 운영보고서
-              </h3>
+              <h3 style={{ fontSize: "30px" }}>{props.areaTitle} 운영보고서</h3>
             </div>
           </div>
           <div className={"pl-4 pr-4 pt-4"}>
-            <div style={{width: "1650px"}}>
-              {props.levelStrStat}
-            </div>
+            <div style={{ width: "1650px" }}>{props.levelStrStat}</div>
           </div>
-
           <CRow>
             <div className="position-absolute-ct">
-              {
-                props.areaTitle === "대전중앙시장" && <h3 className="m-0" style={{fontSize: "30px"}}>- 5 -</h3>
-              }
-              {
-                props.areaTitle !== "대전중앙시장" && <h3 className="m-0" style={{fontSize: "30px"}}>- 4 -</h3>
-              }
+              {props.areaTitle === "대전중앙시장" && <h3 className="m-0" style={{ fontSize: "30px" }}>- 5 -</h3>}
+              {props.areaTitle !== "대전중앙시장" && <h3 className="m-0" style={{ fontSize: "30px" }}>- 4 -</h3>}
             </div>
-
             <div className="position-absolute-rb">
               <img src={"/fs/img/logo-fs.png"} style={{width: "100%", maxWidth: "150px" }} />
             </div>
@@ -185,33 +141,24 @@ export const ComponentToPrint = React.forwardRef((props, ref) => (
           <div className={"d-flex justify-content-between logoMargin"}>
             <img src={'/fs/img/logo.png'} style={{ width: "250px" }} />
             <div className={"p-5"}>
-              <h3 style={{fontSize: "30px"}}>
-                {props.areaTitle} 운영보고서
-              </h3>
+              <h3 style={{ fontSize: "30px" }}>{props.areaTitle} 운영보고서</h3>
             </div>
           </div>
-
           <div className={"pl-4 pr-4 pt-4"}>
-            <div style={{width: "1650px"}}>
+            <div style={{ width: "1650px" }}>
               {props.areaKwhStat}
               {props.areaKwhYearStat}
               {props.areaKwhHourlyStat}
               {props.strKwhStat}
             </div>
           </div>
-
           <CRow>
             <div className="position-absolute-ct">
-              {
-                props.areaTitle === "대전중앙시장" && <h3 className="m-0" style={{fontSize: "30px"}}>- 6 -</h3>
-              }
-              {
-                props.areaTitle !== "대전중앙시장" && <h3 className="m-0" style={{fontSize: "30px"}}>- 5 -</h3>
-              }
+              {props.areaTitle === "대전중앙시장" && <h3 className="m-0" style={{ fontSize: "30px" }}>- 6 -</h3>}
+              {props.areaTitle !== "대전중앙시장" && <h3 className="m-0" style={{ fontSize: "30px" }}>- 5 -</h3>}
             </div>
-
             <div className="position-absolute-rb">
-              <img src={"/fs/img/logo-fs.png"} style={{width: "100%", maxWidth: "150px" }} />
+              <img src={"/fs/img/logo-fs.png"} style={{ width: "100%", maxWidth: "150px" }} />
             </div>
           </CRow>
         </div>
@@ -220,30 +167,19 @@ export const ComponentToPrint = React.forwardRef((props, ref) => (
           <div className={"d-flex justify-content-between logoMargin"}>
             <img src={'/fs/img/logo.png'} style={{ width: "250px" }} />
             <div className={"p-5"}>
-              <h3 style={{fontSize: "30px"}}>
-                {props.areaTitle} 운영보고서
-              </h3>
+              <h3 style={{ fontSize: "30px" }}>{props.areaTitle} 운영보고서</h3>
             </div>
           </div>
-
           <div className={"pl-4 pr-4 pt-4"}>
-            <div style={{width: "1650px"}}>
-              {props.strKwhList}
-            </div>
+            <div style={{width: "1650px"}}>{props.strKwhList}</div>
           </div>
-
           <CRow>
             <div className="position-absolute-ct">
-              {
-                props.areaTitle === "대전중앙시장" && <h3 className="m-0" style={{fontSize: "30px"}}>- 7 -</h3>
-              }
-              {
-                props.areaTitle !== "대전중앙시장" && <h3 className="m-0" style={{fontSize: "30px"}}>- 6 -</h3>
-              }
+              {props.areaTitle === "대전중앙시장" && <h3 className="m-0" style={{ fontSize: "30px" }}>- 7 -</h3>}
+              {props.areaTitle !== "대전중앙시장" && <h3 className="m-0" style={{ fontSize: "30px" }}>- 6 -</h3>}
             </div>
-
             <div className="position-absolute-rb">
-              <img src={"/fs/img/logo-fs.png"} style={{width: "100%", maxWidth: "150px" }} />
+              <img src={"/fs/img/logo-fs.png"} style={{ width: "100%", maxWidth: "150px" }} />
             </div>
           </CRow>
         </div>
