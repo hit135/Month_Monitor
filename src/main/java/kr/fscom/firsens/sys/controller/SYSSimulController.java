1cfe4cf9c68e40a2c2c20496ed1183cb7fbf9501
Hyunmin Uhm <hyunminuhm@gmail.com>
2022-08-02 17:08:52 +0900

    상인회 IGR 메시지 추가

--- src/main/java/kr/fscom/firsens/sys/controller/SYSSimulController.java
+++ src/main/java/kr/fscom/firsens/sys/controller/SYSSimulController.java
@@ -137,7 +137,7 @@ public class SYSSimulController {
         return rtn;
     }
 
-     /**
+    /**
      * @Method Name : sendRptSimulPush
      * @작성일 : 2022-03-28
      * @작성자 : uhm
@@ -180,6 +180,179 @@ public class SYSSimulController {
                 System.out.println("(" + (i+1) + ")");
                 for (Map.Entry entry : message.getKakaoOptions().getVariables().entrySet())
                     System.out.println(entry);
+                System.out.println("========================================================================");
+
+                messageList.add(message);
+            }
+
+            /*
+            MultipleMessageSendingRequest request = new MultipleMessageSendingRequest(messageList);
+            request.setAllowDuplicates(true);
+            MultipleMessageSentResponse response = this.messageService.sendMany(request);
+
+            if (response != null 
+                    && StringUtils.isNotBlank(response.getGroupId())
+                    && StringUtils.isNotBlank(response.getAccountId()))
+                result = true;
+            */
+        } catch (NullPointerException | IllegalArgumentException e) {
+            LOG.debug("sendSimulPush : " + e.getMessage());
+        } catch (Exception e) {
+            LOG.debug("sendSimulPush : " + e.getMessage());
+        } finally {
+            rtn.put("result", result);
+        }
+
+        return rtn;
+    }
+
+    /**
+     * @Method Name : insertIgrEvtDailySimulPush
+     * @작성일 : 2022-08-02
+     * @작성자 : uhm
+     * @변경이력 :
+     * @Method 설명 : 상인회 IGR 이벤트 데이터 추가
+     * @return HashMap<String, Object>
+     */
+    @RequestMapping(value = "/insertIgrEvtDailySimulPush")
+    @ResponseBody
+    public HashMap<String, Object> insertIgrEvtDailySimulPush(HttpServletRequest req) throws Exception {
+        HashMap<String, Object> rtn = new HashMap<>();
+        boolean result = false;
+
+        try {
+            HashMap<String, Object> param = new HashMap() {{
+                put("rptDate", "2022-08-01(월)");
+                put("monitoringDate", "2022-08-01(월)");
+                put("regDate", "2022-08-01");
+                put("logTable", "F_SENSOR_LOG");
+            }};
+
+            int ins1 = this.sysSimulRepo.INSERT_SYS_YESTERDAY_IGREVT_DANGER_DAILY(param);
+            int ins2 = this.sysSimulRepo.UPDATE_SYS_YESTERDAY_IGREVT_WARN_DAILY(param);
+        } catch (NullPointerException | IllegalArgumentException e) {
+            LOG.debug("sendSimulPush : " + e.getMessage());
+        } catch (Exception e) {
+            LOG.debug("sendSimulPush : " + e.getMessage());
+        } finally {
+            rtn.put("result", result);
+        }
+
+        return rtn;
+    }
+
+    /**
+     * @Method Name : sendIgrEvtDailySimulPush
+     * @작성일 : 2022-08-02
+     * @작성자 : uhm
+     * @변경이력 :
+     * @Method 설명 : 상인회 일일 IGR 이벤트 시물레이션 SMS 보내기
+     * @return HashMap<String, Object>
+     */
+    @RequestMapping(value = "/sendIgrEvtDailySimulPush")
+    @ResponseBody
+    public HashMap<String, Object> sendIgrEvtDailySimulPush(HttpServletRequest req) throws Exception {
+        HashMap<String, Object> rtn = new HashMap<>();
+        boolean result = false;
+
+        try {
+            List<HashMap<String, Object>> smsList = this.sysSimulRepo.LIST_SYS_IGREVT_DAILY_PUSH();
+
+            ArrayList<Message> messageList = new ArrayList<>();
+
+            for (int i = 0; i < smsList.size(); i++) {
+                HashMap<String, Object> item = smsList.get(i);
+
+                KakaoOption kakaoOption = new KakaoOption();
+                kakaoOption.setPfId(env.getProp("smsInfo.pfId"));
+                kakaoOption.setTemplateId(env.getProp("smsInfo.templateId3"));
+
+                HashMap<String, String> variables = new HashMap<>();
+                variables.put("#{보고일}", (String) item.get("rptDate"));
+                variables.put("#{시장명}", (String) item.get("areaName"));
+                variables.put("#{모니터링기간}", (String) item.get("monitoringDate"));
+                variables.put("#{발생정보}", (String) item.get("evtInfo"));
+                variables.put("#{발생상점수}", String.valueOf((int) item.get("strCnt")) + "상점");
+                variables.put("#{발생상점목록}", (String) item.get("strList"));
+                kakaoOption.setVariables(variables);
+
+                Message message = new Message();
+                message.setFrom("0424715215");
+                message.setTo(((String) item.get("areaTel")).replaceAll("-", ""));
+                message.setKakaoOptions(kakaoOption);
+
+                System.out.println("========================================================================");
+                System.out.println("(" + (i+1) + ")");
+                for (Map.Entry entry : message.getKakaoOptions().getVariables().entrySet())
+                    System.out.println(entry);
+                System.out.println(message.getTo());
+                System.out.println("========================================================================");
+
+                messageList.add(message);
+            }
+
+            /*
+            MultipleMessageSendingRequest request = new MultipleMessageSendingRequest(messageList);
+            request.setAllowDuplicates(true);
+            MultipleMessageSentResponse response = this.messageService.sendMany(request);
+
+            if (response != null 
+                    && StringUtils.isNotBlank(response.getGroupId())
+                    && StringUtils.isNotBlank(response.getAccountId()))
+                result = true;
+            */
+        } catch (NullPointerException | IllegalArgumentException e) {
+            LOG.debug("sendSimulPush : " + e.getMessage());
+        } catch (Exception e) {
+            LOG.debug("sendSimulPush : " + e.getMessage());
+        } finally {
+            rtn.put("result", result);
+        }
+
+        return rtn;
+    }
+
+      /**
+     * @Method Name : sendIgrEvtDailySimulPush
+     * @작성일 : 2022-08-02
+     * @작성자 : uhm
+     * @변경이력 :
+     * @Method 설명 : 상인회 주별 IGR 이벤트 시물레이션 SMS 보내기
+     * @return HashMap<String, Object>
+     */
+    @RequestMapping(value = "/sendIgrEvtWeekSimulPush")
+    @ResponseBody
+    public HashMap<String, Object> sendIgrEvtWeekSimulPush(HttpServletRequest req) throws Exception {
+        HashMap<String, Object> rtn = new HashMap<>();
+        boolean result = false;
+
+        try {
+            List<HashMap<String, Object>> smsList = this.sysSimulRepo.LIST_SYS_IGREVT_WEEK_PUSH();
+
+            ArrayList<Message> messageList = new ArrayList<>();
+
+            for (int i = 0; i < smsList.size(); i++) {
+                HashMap<String, Object> item = smsList.get(i);
+
+                KakaoOption kakaoOption = new KakaoOption();
+                kakaoOption.setPfId(env.getProp("smsInfo.pfId"));
+                kakaoOption.setTemplateId(env.getProp("smsInfo.templateId4"));
+
+                HashMap<String, String> variables = new HashMap<>();
+                variables.put("#{보고일}", (String) item.get("rptDate"));
+                variables.put("#{시장명}", (String) item.get("areaName"));
+                kakaoOption.setVariables(variables);
+
+                Message message = new Message();
+                message.setFrom("0424715215");
+                message.setTo(((String) item.get("areaTel")).replaceAll("-", ""));
+                message.setKakaoOptions(kakaoOption);
+
+                System.out.println("========================================================================");
+                System.out.println("(" + (i+1) + ")");
+                for (Map.Entry entry : message.getKakaoOptions().getVariables().entrySet())
+                    System.out.println(entry);
+                System.out.println(message.getTo());
                 System.out.println("========================================================================");
 
                 messageList.add(message);
--- src/main/java/kr/fscom/firsens/sys/repository/SYSSimulRepo.java
+++ src/main/java/kr/fscom/firsens/sys/repository/SYSSimulRepo.java
@@ -19,8 +19,10 @@ public interface SYSSimulRepo {
     List<HashMap<String, Object>> LIST_SYS_RPT_PUSH() throws Exception;
     List<HashMap<String, Object>> LIST_SYS_IGREVT_PUSH() throws Exception;          // 상점주
 
-    List<HashMap<String, Object>> LIST_SYS_IGREVT_DAILY_PUSH() throws Exception;    // 상인회 일별
-    List<HashMap<String, Object>> LIST_SYS_IGREVT_WEEK_PUSH() throws Exception;     // 상인회 주별
+    int INSERT_SYS_YESTERDAY_IGREVT_DANGER_DAILY(HashMap<String, Object> param) throws Exception;   // 상인회 일별 주의 daily
+    int UPDATE_SYS_YESTERDAY_IGREVT_WARN_DAILY(HashMap<String, Object> param) throws Exception;     // 상인회 일별 경고 daily
+    List<HashMap<String, Object>> LIST_SYS_IGREVT_DAILY_PUSH() throws Exception;                    // 상인회 일별 push
+    List<HashMap<String, Object>> LIST_SYS_IGREVT_WEEK_PUSH() throws Exception;                     // 상인회 주별 push
 
     int INSERT_SYS_PUSH(HashMap<String, Object> param) throws Exception;
 
